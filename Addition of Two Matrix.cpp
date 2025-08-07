108ec185a220523d533a83023e010a492d46a91a
Shifatpreet <shifatpreetsingh@gmail.com>
2025-08-07 16:04:24 +0530

    Create Addition of Two Matrix.cpp

---
+++ Addition of Two Matrix.cpp
@@ -0,0 +1,62 @@
+//SHIFTPREET SINGH
+//24070123098
+//ENTC B1 
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+    int mat1[100][100];  
+    
+    
+    cout << "Enter the elements of the matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> mat1[i][j];
+        }
+    }
+    
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+    int mat2[100][100];  
+    
+    cout << "Enter the elements of the matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> mat2[i][j];
+        }
+    }
+    cout << "The matrix you have entered:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << mat1[i][j] + mat2[i][j]  << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+/*
+Enter the number of rows: 2
+Enter the number of columns: 2
+Enter the elements of the matrix:
+1 2
+3 4
+Enter the number of rows: 2
+Enter the number of columns: 2
+Enter the elements of the matrix:
+4 5
+6 7
+The matrix you have entered:
+5 7 
+9 11 
+*/
+
